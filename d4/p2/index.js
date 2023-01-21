69c6739fd1e16a0a66ae902a509998521ba7d63f
LK-CeM <lucas.kunz94@gmail.com>
2023-01-21 15:42:43 +0000

    d4 complete

---
+++ d4/p2/index.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var file = fs.readFileSync('../input', 'utf-8');
+var matchList = file.split(/(,|-|\n)/).map(Number)
+let numberList = []
+for (let i in matchList){
+    if (matchList[i] > 0){
+        numberList.push(matchList[i]);
+    }
+}
+counter = 0;
+for (let i  = 0; i < numberList.length; i+= 4){
+    if (numberList[i] <= numberList[i+3] && numberList[i+1] >=numberList[i+2]){
+        counter += 1;
+    }
+    else if (numberList[i] >= numberList[i+3] && numberList[i+1] <= numberList[i+2]){
+        counter += 1;
+    }
+}
+console.log(counter)
