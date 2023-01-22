fb104cbace2b360e55030138fb9b33df14ca2e85
LK-CeM <lucas.kunz94@gmail.com>
2023-01-22 08:42:14 +0000

    d5 done

---
+++ d5/p2/index.js
@@ -0,0 +1,36 @@
+var fs = require('fs');
+var file = fs.readFileSync('../input', 'utf-8');
+var instructionList = file.split(/\r?\n/)
+// 'move 1 from 7 to 4'
+let stack = [["B","Z","T"],
+    ["V","H","T","D","N"],
+    ["B","F","M","D"],
+    ["T","J","G","W","V","Q","L"],
+    ["W","D","G","P","V","F","Q","M"],
+    ["V","Z","Q","G","H","F","S"],
+    ["Z","S","N","R","L","T","C","W"],
+    ["Z","H","W","D","J","N","R","M"],
+    ["M","Q","L","F","D","S"]]
+
+
+
+for (let i in instructionList){
+    instruction = instructionList[i].split(" ").map(Number)
+    let filterInstruction = instruction.filter(function (value) {
+        return !Number.isNaN(value);
+    });
+    let crateList = []
+    for (let j = 0; j < filterInstruction[0]; j++){
+        let crate = stack[filterInstruction[1]-1].pop();
+        crateList.push(crate);
+    }
+    crateList.reverse();
+    for (i in crateList){
+        stack[filterInstruction[2]-1].push(crateList[i]);
+    }
+}
+let result = ""
+for (let i in stack){
+    result+= stack[i].pop();
+}
+console.log(result)
